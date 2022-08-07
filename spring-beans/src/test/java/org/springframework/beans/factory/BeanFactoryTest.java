ae1d2e27842112cba4c3b3c4220bd3f799d2c229
“deep” <1870752227@qqcom>
2022-08-07 11:25:22 +0800

    Initial commit

---
+++ spring-beans/src/test/java/org/springframework/beans/factory/BeanFactoryTest.java
@@ -0,0 +1,23 @@
+package org.springframework.beans.factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * BeanFactory测试
+ *
+ * @author: Deep
+ * @create: 2022-08-07
+ */
+
+class BeanFactoryTest {
+
+
+	@Test
+	void getBean() {
+		BeanFactory factory = new DefaultListableBeanFactory();
+
+	}
+}
